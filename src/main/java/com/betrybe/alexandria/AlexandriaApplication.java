fa25ae21b9ca6ffc129f5546534381a723855f5c
mAngelo-dev <m.angelodev@gmail.com>
2023-08-08 17:25:48 -0300

    Puts correct annotations to make application go up.

--- src/main/java/com/betrybe/alexandria/AlexandriaApplication.java
+++ src/main/java/com/betrybe/alexandria/AlexandriaApplication.java
@@ -2,8 +2,12 @@ package com.betrybe.alexandria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.betrybe.alexandria.models.entities")
+@EnableJpaRepositories("com.betrybe.alexandria.models.repositories")
 public class AlexandriaApplication {
 
 	public static void main(String[] args) {
